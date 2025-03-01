464b08851883882263066b77ae357d80fd250e6b
mithun raj <mithunraj1590@gmail.com>
2025-03-01 23:33:55 +0530

    banner content

--- assets/js/main.js
+++ assets/js/main.js
@@ -700,70 +700,70 @@
 
 
 
-    /* cursor area start*/
-    if ($('.cursor').length) {
-        const cursor = document.querySelector(".cursor");
-        const follower = document.querySelector(".cursor-follower");
-        const gsapCursor = document.querySelectorAll(".gsap-cursor");
+    // /* cursor area start*/
+    // if ($('.cursor').length) {
+    //     const cursor = document.querySelector(".cursor");
+    //     const follower = document.querySelector(".cursor-follower");
+    //     const gsapCursor = document.querySelectorAll(".gsap-cursor");
 
-        let posX = 0,
-            posY = 0,
-            mouseX = 0,
-            mouseY = 0;
+    //     let posX = 0,
+    //         posY = 0,
+    //         mouseX = 0,
+    //         mouseY = 0;
 
-        TweenMax.to({}, 0.02, {
-            repeat: -1,
-            onRepeat: function () {
-                posX += (mouseX - posX) / 9;
-                posY += (mouseY - posY) / 9;
+    //     TweenMax.to({}, 0.02, {
+    //         repeat: -1,
+    //         onRepeat: function () {
+    //             posX += (mouseX - posX) / 9;
+    //             posY += (mouseY - posY) / 9;
 
-                TweenMax.set(follower, {
-                    css: {
-                        left: posX - 20,
-                        top: posY - 20
-                    }
-                });
+    //             TweenMax.set(follower, {
+    //                 css: {
+    //                     left: posX - 20,
+    //                     top: posY - 20
+    //                 }
+    //             });
 
-                TweenMax.set(cursor, {
-                    css: {
-                        left: mouseX,
-                        top: mouseY
-                    }
-                });
-            }
-        });
+    //             TweenMax.set(cursor, {
+    //                 css: {
+    //                     left: mouseX,
+    //                     top: mouseY
+    //                 }
+    //             });
+    //         }
+    //     });
 
-        document.addEventListener("mousemove", (e) => {
-            mouseX = e.pageX;
-            mouseY = e.pageY;
-        });
+    //     document.addEventListener("mousemove", (e) => {
+    //         mouseX = e.pageX;
+    //         mouseY = e.pageY;
+    //     });
 
-        gsapCursor.forEach((el) => {
-            el.addEventListener("mouseenter", () => {
-                cursor.classList.add("active");
-                follower.classList.add("active");
-            });
+    //     gsapCursor.forEach((el) => {
+    //         el.addEventListener("mouseenter", () => {
+    //             cursor.classList.add("active");
+    //             follower.classList.add("active");
+    //         });
 
-            el.addEventListener("mouseleave", () => {
-                cursor.classList.remove("active");
-                follower.classList.remove("active");
-            });
-        });
+    //         el.addEventListener("mouseleave", () => {
+    //             cursor.classList.remove("active");
+    //             follower.classList.remove("active");
+    //         });
+    //     });
 
 
-        $(".slider-drag-wrap").hover(function () {
-            $('.cursor').addClass('active');
-        }, function () {
-            $('.cursor').removeClass('active');
-        });
+    //     $(".slider-drag-wrap").hover(function () {
+    //         $('.cursor').addClass('active');
+    //     }, function () {
+    //         $('.cursor').removeClass('active');
+    //     });
 
-        $(".slider-drag-wrap a").hover(function () {
-            $('.cursor').removeClass('active');
-        }, function () {
-            $('.cursor').addClass('active');
-        });
-    }
-    /* cursor area end */
+    //     $(".slider-drag-wrap a").hover(function () {
+    //         $('.cursor').removeClass('active');
+    //     }, function () {
+    //         $('.cursor').addClass('active');
+    //     });
+    // }
+    // /* cursor area end */
 
   
 
